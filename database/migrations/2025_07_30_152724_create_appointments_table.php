9df13c86986f32e0c7b0775aa519bb21b5ce3c70
Pranto Biswas <prantobd320@gmail.com>
2025-07-31 00:53:51 +0600

    first commit

---
+++ database/migrations/2025_07_30_152724_create_appointments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained()->onDelete('cascade');
+            $table->string('service');
+            $table->timestamp('appointment_time');
+            $table->integer('duration')->default(60); // in minutes
+            $table->enum('attendance_status', ['attended', 'canceled', 'no_show'])->nullable();
+            $table->enum('status', ['Scheduled', 'Confirmed', 'Canceled'])->default('Scheduled');
+            $table->boolean('reminder_sent')->default(false); // optional for reminder tracking
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
